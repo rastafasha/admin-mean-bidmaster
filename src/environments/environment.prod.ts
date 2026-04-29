51538bd8239ff45ad2c6223fecaf5c650fa854b0
Malcolm Cordova <41023438+rastafasha@users.noreply.github.com>
2026-04-29 19:44:48 -0400

    corregido url backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  apiUrl: "https://backend-bidmaster-mean.vercel.app/api",
+  apiUrl: "https://backend-bidmaster-mean.onrender.com/api",
   apiUrlMedia: 'https://res.cloudinary.com/dmv6aukai/image/upload/v1741218430/articlesApp/uploads',
 
 };
